c90369b8b7cf56865be3eb9348da4f32c84cb623
indian-command <nagnishal06@gmail.com>
2026-06-22 09:40:34 +0530

    Update Google Maps embeds to search-based URLs for Anantapur

--- src/views.js
+++ src/views.js
@@ -366,7 +366,7 @@ function compileContactView() {
           <!-- Styled Maps Widget -->
           <div class="map-wrapper-large">
             <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61845.54911477759!2d77.56846153724813!3d14.665569429402685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb14ac43bf182fb%3A0x6b449175402de33!2sAnantapur%2C%20Andhra%20Pradesh!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin" 
+              src="https://maps.google.com/maps?q=Anantapur,%20Andhra%20Pradesh%20515004&t=&z=14&ie=UTF8&iwloc=&output=embed" 
               width="100%" 
               height="100%" 
               style="border:0;" 
